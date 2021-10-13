90a4a93ac702e5c4970782b47c7b9f4b3e558afe
bell-rabbit <andy19974a22@gmail.com>
2021-10-13 22:03:38 +0800

    feat(App) add GithubCorner.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <template>
   <v-app :style='getBackgroundImage()'>
+    <github-corner v-if="$vuetify.breakpoint.width > 650"/>
     <v-main>
       <v-container style="background-color: #ffffff; max-width: 450px" class="pa-0">
         <v-app-bar flat dense
                    :src="require('./assets/top.jpg')"
         >
-          <v-toolbar-title class="white--text">Collapsing Bar</v-toolbar-title>
+          <v-toolbar-title class="white--text">彈珠跳台動畫模擬器</v-toolbar-title>
         </v-app-bar>
         <router-view/>
       </v-container>
@@ -14,10 +15,10 @@
 </template>
 
 <script>
+import GithubCorner from './components/GithubCorner';
 export default {
   name: 'App',
-  components: {
-  },
+  components: { GithubCorner },
   methods: {
     getBackgroundImage () {
       return {
