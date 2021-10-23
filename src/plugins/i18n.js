b296f0964d1e44ca306782aa8f59b85797fb1b12
bell-rabbit <andy19974a22@gmail.com>
2021-10-23 15:35:30 +0800

    style(core) update the code style

--- src/plugins/i18n.js
+++ src/plugins/i18n.js
@@ -16,5 +16,5 @@ const messages = {
 export default new VueI18n({
   locale: process.env.VUE_APP_I18N_LOCALE || "zh-tw",
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "zh-tw",
-  messages: messages
+  messages
 });
