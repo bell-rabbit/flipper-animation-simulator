6d07f82f0eca4cf6befec7e58777a458abad577c
bell-rabbit <andy19974a22@gmail.com>
2021-10-23 15:59:42 +0800

    refactor(shareUrl):

--- src/components/RollButton.vue
+++ src/components/RollButton.vue
@@ -1,10 +1,10 @@
 <template>
   <v-btn depressed height="55px" width="178px" @click="clickButton" :loading="loading" :style="getBackgroundImage()">
     <template v-slot:loader>
-        <v-progress-circular
-            indeterminate
-            color="#ea3653"
-        ></v-progress-circular>
+      <v-progress-circular
+        indeterminate
+        color="#ea3653"
+      ></v-progress-circular>
     </template>
   </v-btn>
 </template>
@@ -31,18 +31,8 @@ export default {
       let url = this.alert ? require("../assets/alertRoll.png") : require("../assets/roll.png");
 
       if (!this.alert) {
-        switch (this.star) {
-          case 1:
-            break;
-          case 3:
-            url = require("../assets/roll_3.png");
-            break;
-          case 4:
-            url = require("../assets/roll_4.png");
-            break;
-          case 5:
-            url = require("../assets/roll_5.png");
-            break;
+        if (this.star !== 1) {
+          url = require(`../assets/roll_${this.star}.png`);
         }
       }
 
